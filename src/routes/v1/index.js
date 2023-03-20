ecf5f77724c8143f0e4d0e48038e9c003eede087
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2023-03-21 01:57:30 +0530

    Added error code enums

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -12,7 +12,11 @@ router.get('/city/:id' , CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
 
-router.post('/flights',FlightMiddlewares.validateCreateFlight, FlightController.create);  
+router.post(
+    '/flights',
+    FlightMiddlewares.validateCreateFlight, 
+    FlightController.create
+);  
 router.get('/flights', FlightController.getAll);
 
 router.post('/airports', AirportController.create);
---
+++ src/utils/error-codes.js
@@ -0,0 +1,22 @@
+const ClientErrorsCodes = Object.freeze({
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404
+});
+
+const ServerErrorsCodes = Object.freeze({
+    INTERNAL_SERVER_ERROR: 500,
+    NOT_IMPLEMENTED: 501, 
+});
+
+const SucessCodes = Object.freeze({
+    OK: 200,
+    CREATED: 201
+});
+
+
+module.exports = {
+    ClientErrorsCodes,
+    ServerErrorsCodes,
+    SucessCodes
+}
